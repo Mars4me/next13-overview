2f0a5f9278e3b059c2ccfca002c1ae5315ff3204
Mars4me <lmarsl1994@gmail.com>
2023-01-25 16:19:00 +0200

    test

--- app/Header.tsx
+++ app/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
         <header className="p-5 bg-blue-500 text-white">
             <p>I'm a Header</p>
             <nav className="flex gap-1.5">
-                <Link href="/" className="bg-white text-blue-500 px-2  rounded-md">
+                <Link href="/" className="bg-white text-blue-500 px-2 rounded-md">
                     Home
                 </Link>
                 <Link href="/todos" className="bg-white text-blue-500 px-2 rounded-md">
